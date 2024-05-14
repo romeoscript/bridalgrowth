334cb101daa543af8fb7ae58f6efcfe8458ec229
romeoscript <romeobourne211@gmail.com>
2024-05-14 23:09:30 +0100

    condesence

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 import Hero from "@/Components/Hero";
 import Partners from "@/Components/Partners";
+import Result from "@/Components/Result";
 import Service from "@/Components/Service";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
@@ -9,6 +11,7 @@ export default function Home() {
       <Hero />
       <Partners />
       <Service />
+      <Result />
     </main>
   );
 }
